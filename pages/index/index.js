eb755edf32ccd1ff4025a70bd4a281ef15912c05
choe <348863046@qq.com>
2018-08-09 17:53:38 +0800

    首页页面编写

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var requestUtils = require('../../utils/request.js')
 
 Page({
   data: {
@@ -8,12 +9,22 @@ Page({
     autoplay: true,  //是否自动播放
     interval: 5000, //轮播间隔
     current:0, // 当前是第几个滑块
-    duration: 1000, //滑动动画时长
-    circular: true, //是否采用衔接滑动
+    duration: 500, //滑动动画时长
+    circular: false, //是否采用衔接滑动
+    imgUrls:null
   },
   
   onLoad: function () {
-    
+    var that = this;
+    requestUtils.doPost(app.globalData.baseUrl,700,"",
+    function(res){
+      that.setData({
+        imgUrls: res.data.info
+      });
+    },
+    function(res){
+
+    })
   }
   
 })
